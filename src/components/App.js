4298b4322224c5e0cea35d0ca2c6abcb67d2893f
carolinagap <csaenz@growthaccelerationpartners.com>
2018-10-30 10:31:34 -0500

    Add static notes

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,39 @@
 import React, { Component } from 'react';
+import Note from './Note.js';
 import './App.css';
 
 class App extends Component {
+  state = {
+    notes: [
+      {
+        title: 'My first note',
+        content: 'Content content content content'
+      },
+      {
+        title: 'My second note',
+        content: 'Content content content content'
+      },
+      {
+        title: 'My third note',
+        content: 'Content content content content'
+      }
+    ]
+  };
+
   render() {
+    const notes = this.state.notes.map((note) => (
+      <Note
+        title={note.title}
+        content={note.content}
+      />
+    ));
+
     return (
       <div className="app">
         <header className="app-header">
-          Hello World
+          Evertrello
         </header>
+        {notes}
       </div>
     );
   }
---
+++ src/components/Note.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './Note.css';
+
+class Note extends Component {
+  render() {
+    return (
+      <div className="note">
+        <header className="note-header">
+          {this.props.title}
+        </header>
+        <div className="note-content">
+          {this.props.content}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Note;
